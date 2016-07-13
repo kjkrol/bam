6e9b6f745778a2d0557224ff2afb008c0d01d6a1
Krol, Karol <Karol.Krol@vimn.com>
2016-07-13 19:48:55 +0200

    simplify native libs loader

--- engine/src/main/java/bam/common/NativeLibLoader.java
+++ engine/src/main/java/bam/common/NativeLibLoader.java
@@ -1,35 +1,43 @@
 package bam.common;
 
-import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Slf4j
 class NativeLibLoader {
+
     private static final String JAVA_IO_TMPDIR = "java.io.tmpdir";
     private static final String JAVA_LIBRARY_PATH = "java.library.path";
     private static final String SYS_PATHS = "sys_paths";
-    private final Path libraryPath;
 
     private File osTempDirectory;
 
-    @Builder
-    private NativeLibLoader(final Path libraryPath) {
-        this.libraryPath = libraryPath;
+    private boolean successfullyInitialized;
+
+    NativeLibLoader() {
+        init();
     }
 
-    void loadLibraries() {
+    void addFileToJavaLibraryPath(Path libraryPath) {
+        if (successfullyInitialized) {
+            final File tempFile = new File(osTempDirectory.getAbsolutePath() + "/" + libraryPath.getFileName().toString());
+            try {
+                Files.copy(libraryPath, tempFile.toPath());
+                tempFile.deleteOnExit();
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+    }
+
+    private void init() {
         if (isTempDirectoryExist()) {
-            saveLibraryIntoTemporaryOsSpace();
-            addLibsToJavaLibraryPath();
+            successfullyInitialized = setupJavaLibraryPath();
         }
     }
 
@@ -39,47 +47,17 @@ class NativeLibLoader {
         return osTempDirectory.exists() || osTempDirectory.mkdir();
     }
 
-    private void saveLibraryIntoTemporaryOsSpace() {
-        try {
-            saveLibraryIntoTemporaryOsSpace(libraryPath);
-        } catch (UnsatisfiedLinkError | IOException e) {
-            log.error("Native library: {} failed to load.\n", libraryPath);
-            log.error(e.getMessage(), e);
-            System.exit(1);
-        }
-    }
 
-    private void saveLibraryIntoTemporaryOsSpace(final Path libraryPath) throws IOException {
-        final InputStream in = initInputStreamToResourceFromJar(libraryName);
-        final OutputStream out = initOutputStreamToTemporaryFile(libraryName);
-        IOUtils.copy(in, out);
-    }
-
-    private InputStream initInputStreamToResourceFromJar(final String libraryName) {
-        return NativeLibLoader.class.getClassLoader().getResourceAsStream(path);
-    }
-
-    private OutputStream initOutputStreamToTemporaryFile(final String libraryName) throws IOException {
-        final File temporaryFile = createTemporaryFile(libraryName);
-        return new FileOutputStream(temporaryFile);
-    }
-
-    private File createTemporaryFile(final String libraryName) throws IOException {
-        final File tempFile = new File(osTempDirectory, libraryName);
-        if (tempFile.createNewFile()) {
-            tempFile.deleteOnExit();
-        }
-        return tempFile;
-    }
-
-    private void addLibsToJavaLibraryPath() {
+    private boolean setupJavaLibraryPath() {
         try {
             System.setProperty(JAVA_LIBRARY_PATH, osTempDirectory.getAbsolutePath());
             final Field fieldSysPath = ClassLoader.class.getDeclaredField(SYS_PATHS);
             fieldSysPath.setAccessible(true);
             fieldSysPath.set(null, null);
+            return true;
         } catch (IllegalAccessException | NoSuchFieldException e) {
             log.error(e.getMessage(), e);
         }
+        return false;
     }
 }
--- engine/src/main/java/bam/opengl/OpenGlLoader.java
+++
@@ -1,9 +0,0 @@
-package bam.opengl;
-
-import bam.common.CrossPlatformLibsLoader;
-
-public class OpenGlLoader {
-    public void load() {
-        new CrossPlatformLibsLoader().loadNativeLibs();
-    }
-}
